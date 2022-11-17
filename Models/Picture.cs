d17fe48b5f71c2d18f20b956d68038c726fb3da9
m13303620317 <116149426+m13303620317@users.noreply.github.com>
2022-11-17 17:19:49 +0800

    Add files via upload

---
+++ Models/Picture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21App.Models
+{
+    public class Picture
+    {
+        public string PicturePath { get; set; }
+    }
+
+    public class PictureManager
+    {
+        public static List<Picture> GetPictures()
+        {
+            var pictures = new List<Picture>();
+            pictures.Add(new Picture { PicturePath = "Assets/a.jpg" });
+            pictures.Add(new Picture { PicturePath = "Assets/b.jpg" });
+            pictures.Add(new Picture { PicturePath = "Assets/c.jpg" });
+
+            return pictures;
+        }
+    }
+}
---
+++ Models/PictureMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21App.Models
+{
+    public class PictureMain
+    {
+        public string PicturePath { get; set; }
+    }
+
+    public class PictureMainManager
+    {
+        public static List<PictureMain> GetPictureMainList()
+        {
+            var pictures = new List<PictureMain>();
+
+            pictures.Add(new PictureMain { PicturePath = "Assets/a.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/b.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/c.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/d.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/e.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/f.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/g.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/h.jpg" });
+            pictures.Add(new PictureMain { PicturePath = "Assets/i.jpg" });
+
+            return pictures;
+        }
+    }
+}
